b2644116e483de3cbe5034dd113622150901be7f
Saifobai <saifobaidi.s22@gmail.com>
2021-08-18 14:11:32 +0200

    i added some folders to client side

---
+++ client/src/context.js
@@ -0,0 +1,28 @@
+
+
+import  {useState, createContext} from 'react';
+
+
+export const FirstContext = createContext();
+
+
+
+
+export default function PayContext() {
+    return (
+       
+
+        <payContext.Provider value={{
+
+        }}>
+
+            {children}
+        </payContext.Provider>
+
+    )
+}
+
+
+
+
+

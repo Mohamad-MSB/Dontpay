eacc7b33161de03b2b0f228bbd3df9dad86e8f0e
Saifobai <saifobaidi.s22@gmail.com>
2021-09-21 01:23:10 +0200

    still working on it

--- client/src/components/scrolUp/ScrollToTop.js
+++ client/src/components/scrolUp/ScrollToTop.js
@@ -1,11 +1,24 @@
 
 
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useWindowScroll } from "react-use";
+
 import './scroll.scss';
 import { FaChevronUp } from 'react-icons/fa';
 
 
 function ScrollToTop() {
+
+
+    const { x, y } = useWindowScroll();
+    const [scrolled, setScrolled] = useState(0);
+
+    useEffect(() => {
+        const height =
+            document.documentElement.scrollHeight -
+            document.documentElement.clientHeight;
+        setScrolled((y / height) * 100);
+    }, [y]);
 
     return (
 

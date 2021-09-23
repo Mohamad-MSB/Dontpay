d558e8e52c5de162c60f689fae46a5ae08f3a72a
Mohamad <alhussaini21m@gmail.com>
2021-09-23 13:55:57 +0200

    message change

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,43 +1,112 @@
-const messagesModel = require('../models/messagesModel');
+const { Mongoose } = require('mongoose');
+const roomModel = require('../models/roomModel');
+const userModel = require('../models/userModel');
 
 
-
-
-exports.getMessage = async (req, res) => {
+exports.writeMessage = async (req, res) => {
 
     try {
+        const owner = await userModel.findById(req.params.owner); // saif
+
+        let articleRoom = await roomModel.findOne({ article_id: req.params.id, sender: req.user._id }); // mohamad
+        // let articleRoom = await roomModel.findOne({ _id: Mongoose.Types.ObjectId(req.params.room)}); // mohamad
         
-        const message = await messagesModel.find({
-            user1: req.user._id,
-            user2: req.body.userId
-        })
+        // const partnerID = null;
 
-        res.status(200).json({message: message});
+        // if(articleRoom.reciever == req.user._id){
+        //     partnerID = articleRoom.sender;
+        // } else if(articleRoom.sender == req.user._id) {
+        //     partnerID = articleRoom.reciever;
+        // }
 
+        if (articleRoom !== null) {
+            console.log(articleRoom.reciever._id);
+            articleRoom = await roomModel.findOneAndUpdate({ article_id: req.params.id, $or:[{ reciever: owner }, { sender:owner}] },
+                {
+                    $push: {
+                        messages: {
+                            sender: req.user._id,
+                            reciever: owner,
+                            message_body: req.body.message
+                        }
+                    }
+                }, { new: true }
+            )
+        } else {
+            articleRoom = await roomModel.create({
+                article_id: req.params.id,
+                sender: req.user._id,
+                reciever: req.params.owner,
+                messages: [{
+                    sender: req.user._id,
+                    reciever: owner,
+                    message_body: req.body.message
+                }]
+            });
+        }
+        return res.status(200).json({ note: "message send success", message: articleRoom });
+    } catch (error) {
+        res.status(500).json({ message: "error happens here", error: error.message })
+    }
+}
+
+exports.showMessages = async (req, res) => {
+
+    try {
+        const messages = await roomModel.find({ $or:[{ reciever: req.user._id }, { sender:req.user._id}]}).populate({path:"article_id messages sender reciever", populate: {path: "sender reciever"}});
+                                                                            
+       return res.status(200).json({ messages: messages, });
 
     } catch (error) {
+       return res.status(500).json({ message: "error happens here", error: error.message })
+    }
+}
+
+exports.singleChatRoom = async (req, res) => {
+
+    try {
+
+        const chat = await roomModel.findOne({ article_id : req.params.id, $or:[{ reciever: req.user._id }, { sender:req.user._id}]}).populate({path:"article_id messages sender reciever", populate: {path: "sender reciever"}});
+
+        return res.status(200).json({ messages: "single chat room", chat: chat.messages,id: chat._id });
+        
+    } catch (error) {
+        
+       return res.status(500).json({ message: "error happens here", error: error.message })
         
     }
 }
 
-
-exports.sendMessage = async(req, res) => {
-    console.log(req.body.userId, )
-
+exports.replayMessage = async (req, res) => {
     try {
-        
-        const message = await messagesModel.create({
-            sender: req.user._id,
-            reciever: req.params.id,
-            content: req.body.message
-        })
 
-        
-        res.status(200).json({message: "message send", message: message})
-
-
-
+        const articleRoom = await roomModel.findByIdAndUpdate(req.params.id,
+                {
+                    $push: {
+                        messages: {
+                            sender: req.user._id,
+                            reciever: req.body.owner,
+                            message_body: req.body.message,
+                            created_at: Date.now()
+                        }
+                    }
+                }, { new: true }
+            )
+        return res.status(200).json({ note: "message send success", message: articleRoom });
     } catch (error) {
-        res.status(500).json({message:"error happens here", error: error.message})
+        res.status(500).json({ message: "error happens here", error: error.message })
     }
+
 }
+
+// exports.showMessages = async (req, res) => {
+
+//     try {
+//         const articleRooms = await roomModel.findOne({ article_id: req.params.id, $or:[{ reciever: req.params.owner }, { sender: req.params.owner},{ reciever: req.user._id }, { sender:req.user._id}]}).populate({path:"messages", populate: {path: "sender"}});
+                                                                            
+//         res.status(200).json({ articleRoom: articleRooms.messages, room: articleRooms });
+
+//     } catch (error) {
+//         res.status(500).json({ message: "error happens here", error: error.message })
+//     }
+// }
